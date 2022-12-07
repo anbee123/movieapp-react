d936da17fe56fdab140b02997b13ebe260e4d8f9
An Bui <anhipchipchip@gmail.com>
2022-12-06 21:50:13 -0500

    first commit, set up the app.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {useState, useEffect} from 'react'
+import './App.css';
+
+function App() {
+  const [movieData, setMovieData] = useState({})
+  const [movieTitle, setMovieTitle] = useState('star wars')
+
+fetch()
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
